016963c35510ed4cb7fb5418d23ffff6d273dab5
Пользователь <suleiman2005@inbox.ru>
2022-11-11 11:12:24 +0300

    Solar_model.py is updated

--- solar_model.py
+++ solar_model.py
@@ -19,8 +19,9 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
+        force = (gravitational_constant*body.m*obj.m) / r**2
+        body.Fx += force * ((obj.x-body.x)/r)
+        body.Fy += force * ((obj.y-body.y)/r)
 
 
 def move_space_object(body, dt):
@@ -32,9 +33,12 @@ def move_space_object(body, dt):
     """
 
     ax = body.Fx/body.m
-    body.x += 42  # FIXME: не понимаю как менять...
+    body.x += Vx*dt
     body.Vx += ax*dt
-    # FIXME: not done recalculation of y coordinate!
+    
+    ay = body.Fy/body.m
+    body.y += Vy*dt
+    body.Vy += ay*dt
 
 
 def recalculate_space_objects_positions(space_objects, dt):
