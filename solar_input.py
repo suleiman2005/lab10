44f2ac4d3c73bbe7d5500688bad7c0e0057b8fa7
Пользователь <suleiman2005@inbox.ru>
2022-11-11 11:30:23 +0300

    Solar_input.py is updated

--- solar_input.py
+++ solar_input.py
@@ -23,11 +23,14 @@ def read_space_objects_data_from_file(input_filename):
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            elif object_type == "planet":  # FIXME: do the same for planet
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
             else:
                 print("Unknown space object")
 
     return objects
-
 
 def parse_star_parameters(line, star):
     """Считывает данные о звезде из строки.
@@ -43,8 +46,14 @@ def parse_star_parameters(line, star):
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
-
-    pass  # FIXME: not done yet
+    line = line.split()
+    star.R = line[1]
+    star.color = line[2]
+    star.m = line[3]
+    star.x = line[4]
+    star.y = line[5]
+    star.Vx = line[6]
+    star.Vy = line[7]
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -61,7 +70,15 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    pass  # FIXME: not done yet...
+
+    line = line.split()
+    planet.R = line[1]
+    planet.color = line[2]
+    planet.m = line[3]
+    planet.x = line[4]
+    planet.y = line[5]
+    planet.Vx = line[6]
+    planet.Vy = line[7]
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
@@ -77,7 +94,10 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
+            if obj.type == "star":
+                print(out_file, " ".join("Star", obj.R, obj.color, obj.m, obj.x, obj.y, obj.Vx, obj.Vy))
+            elif obj.type == "planet":
+                print(out_file, " ".join("Planet", obj.R, obj.color, obj.m, obj.x, obj.y, obj.Vx, obj.Vy))
             # FIXME: should store real values
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
